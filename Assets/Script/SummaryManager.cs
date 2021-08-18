977b5d86ae5365845603f5e6257b6cc59a02cce9
Quentin Bleuse <q.bleuse@student.isartdigital.com>
2021-08-18 21:46:41 +0200

    made scroll snap

--- Assets/Script/SummaryManager.cs
+++ Assets/Script/SummaryManager.cs
@@ -8,14 +8,21 @@ public class SummaryManager : MonoBehaviour
 {
 	/*==== SETTINGS ====*/
 	/* prefabs of the summary contents */
-	[SerializeField]					private SummaryContainer	_summaryGo		= null;
+	[SerializeField]			private SummaryContainer	_summaryGo		= null;
 	/* parent transform of the instanciated Summary Container*/
-	[SerializeField]					private RectTransform		_scroller		= null;
-	[SerializeField,Min(0.0f)]	private float						_verticalOffset = 0.0f;
+	[SerializeField]			private RectTransform		_scroller		= null;
+	[SerializeField]			private RectTransform		_middleTrs		= null;
+	[SerializeField,Min(0.0f)]	private float				_verticalOffset = 0.0f;
+	[SerializeField,Min(0.1f)]	private float				_lerpDuration	= 1.0f;
 
 	/*==== COMPONENTS ====*/
-	public List<SummaryContainer>               _summaries		= new List<SummaryContainer>();
-	private StringBuilder						_stringBuilder	= null;
+	public List<SummaryContainer>	_summaries		= new List<SummaryContainer>();
+	private StringBuilder			_stringBuilder	= null;
+
+	/*==== STATE ====*/
+	private int			_currFocus		= 0;
+	private Coroutine	_focusCoroutine = null;
+	private float		_prevScrollerY	= 0.0f;
 
 	public void Init()
 	{
@@ -52,6 +59,41 @@ public class SummaryManager : MonoBehaviour
 		
 		StartCoroutine(PlaceSummaries());
 	}
+
+	public void OnBeginDrag()
+    {
+		if (_focusCoroutine != null)
+			StopCoroutine(_focusCoroutine);
+		/* saving previous y */
+		_prevScrollerY = _scroller.anchoredPosition.y;
+    }
+
+	public void OnEndDrag()
+	{
+		/* we find the closest to focus on, we allow to move only one at a time */
+		int		nextPossibleFocus	= 0;
+		/* we look if the next possible focus is before or after the current index */
+		if (_prevScrollerY < _scroller.anchoredPosition.y)
+			nextPossibleFocus = _currFocus + 1;
+		else
+			nextPossibleFocus = _currFocus - 1;
+
+		if (nextPossibleFocus < 0 || nextPossibleFocus > _summaries.Count)
+        {
+			_focusCoroutine = StartCoroutine(FocusOnSummary());
+			return;
+		}
+
+		if (	Mathf.Abs(_summaries[nextPossibleFocus].transform.position.y - _middleTrs.transform.position.y) 
+			<	Mathf.Abs(_summaries[_currFocus].transform.position.y - _middleTrs.transform.position.y))
+		{
+			_currFocus = nextPossibleFocus;
+		}
+
+		_focusCoroutine = StartCoroutine(FocusOnSummary());
+	}
+
+
 
 	private void FillContent(SummaryContainer sumContainer_, MCQ mcq_)
 	{
@@ -94,6 +136,12 @@ public class SummaryManager : MonoBehaviour
 		
 	}
 
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
 	IEnumerator PlaceSummaries()
     {
 		yield return new WaitForFixedUpdate();
@@ -108,6 +156,7 @@ public class SummaryManager : MonoBehaviour
 
 		for (int i = 0; i < _summaries.Count; i++)
 		{
+
 			y -= (height - _summaries[i].content.rectTransform.rect.height) / 2.0f;
 			_summaries[i].content.rectTransform.anchoredPosition = new Vector2(x, y);
 			y = (-(i+1)*height) + (i+1) * _verticalOffset;
@@ -116,9 +165,21 @@ public class SummaryManager : MonoBehaviour
 		yield break;
 	}
 
-	// Update is called once per frame
-	void Update()
+	IEnumerator FocusOnSummary()
 	{
-		
-	}
+		Vector2 newPos = Vector2.zero;
+		float targetY = -((-(_currFocus) * _scroller.rect.height) + (_currFocus) * _verticalOffset);
+
+		float elapsedTime = 0.0f;
+
+		while(elapsedTime < _lerpDuration)
+        {
+			newPos.y = Mathf.Lerp(_scroller.anchoredPosition.y, targetY, elapsedTime/_lerpDuration);
+			_scroller.anchoredPosition = newPos;
+			elapsedTime += Time.deltaTime;
+			yield return null;
+        }
+
+		yield break;
+    }
 }
